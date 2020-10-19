28ce79932b883aebc0c72261c502ab6975e5f863
Archana <70164005+aneupanetims2016@users.noreply.github.com>
2020-10-19 12:57:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ImportLibrary",
+    version="0.0.1",
+    author="Archana Neupane Timsina",
+    author_email="aneupanetims2016@fau.edu",
+    description="Import everything used in the MPCR lab.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mpcrlab/importmpcr",
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
