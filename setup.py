e01f7ee29a5c431efbbaff48aa9d315cbde0e7f9
Archana <70164005+aneupanetims2016@users.noreply.github.com>
2020-10-19 13:04:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Library",
+    name="ImportLibrary",
     version="0.0.1",
     author="Archana Neupane Timsina",
     author_email="aneupanetims2016@fau.edu",
     description="Import everything used in the MPCR lab.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://https://github.com/aneupanetims2016/ImportLibrary",
+    url="https://https://github.com/aneupanetims2016/Library",
     install_requires=[],
     packages=setuptools.find_packages(),
     classifiers=[
